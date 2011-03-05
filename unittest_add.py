0f846c73499ac8196b65f678fb329280016f1c00
Julian Ceipek <julian.ceipek@gmail.com>
2011-03-05 03:57:39 -0500

    Added basic unittest for placeholder module 'add'

---
+++ unittest_add.py
@@ -0,0 +1,46 @@
+#Do this for every unittest file#
+import unittest
+import xmlrunner
+#-------------------------------#
+
+import add
+
+class testAdd(unittest.TestCase):
+    """
+    A test class for the add module.
+    """
+
+    def setUp(self):
+        """
+        set up data used in the tests.
+        setUp is called before each test function execution.
+        """
+        self.add = add.add
+        self.num1 = 5
+        self.num2 = 11.4
+        self.num3 = -4
+        
+    def tearDown(self):
+        """
+        clean up after any test
+        tearDown is called after each test function execution.
+        """
+
+    def testAddInts(self):
+        self.assertEqual(self.add(self.num1,self.num1), 10)
+
+    def testAddFloats(self):
+        self.assertEqual(self.add(self.num2,self.num2), 22.8)
+        
+    def testAddIntToFloat(self):
+        self.assertEqual(self.add(self.num1,self.num2), 16.4)
+        
+    def testAddNegInts(self):
+        self.assertEqual(self.add(self.num3,self.num3), -8)
+        
+    def testAddPosNegInts(self):
+        self.assertEqual(self.add(self.num1,self.num3), 1)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(testAdd)
+    xmlrunner.XMLTestRunner().run(suite)
