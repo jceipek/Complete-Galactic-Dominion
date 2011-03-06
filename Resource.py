50dc1df46ec4eface5687637bfa246bb24e0d0fc
Karan Kanodia <karankanodia@uniteddemand.com>
2011-03-05 20:55:49 -0500

    HUD.py Resource.py NaturalObject.py StatusInfo.py ObjectProperties.py Minimap.py ResourceView.py

---
+++ Resource.py
@@ -0,0 +1,8 @@
+import NaturalObject
+
+class Resource(NaturalObject):
+    """Gatherable by villagers that is used to make builders"""
+    def __init__(self):
+        NaturalObject.__init__(self)
+        resourceType=none
+                
