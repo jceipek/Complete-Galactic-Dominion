6ccc68674ffb0b24394a8670e4f1be9fb0866be0
Julian Ceipek <julian.ceipek@gmail.com>
2011-03-06 19:36:47 -0500

    Updated SVN script to handle deletions of earlier files

--- svnScript.py
+++ svnScript.py
@@ -1,5 +1,6 @@
 import os
 
+os.popen('rm -rf /home/is/subversionRepo/complete-galactic-dominion/*')
 os.popen('cp -r /home/is/.hudson/jobs/complete-galactic-dominion/workspace/* /home/is/subversionRepo/complete-galactic-dominion/')
 os.popen('svn add /home/is/subversionRepo/complete-galactic-dominion/*')
 gitres = os.popen('git log -1')
