bab3fad3204b8b0e7d96387497b38a5e7a55192d
Julian Ceipek <julian.ceipek@gmail.com>
2011-03-15 20:01:07 -0400

    Added multiprocessing test

---
+++ internal/multiProcessing.py
@@ -0,0 +1,35 @@
+from random import randint
+
+#import pygame
+from multiprocessing import Process, Pipe, Lock
+
+
+#pygame.init()
+
+
+def eventManager(events):
+	print "Event Manager loop"
+	for event in events:
+				print event
+
+def renderer():
+    import pygame
+    screenSize = (width, height) = (640, 480)
+    pygame.init()
+    screen = pygame.display.set_mode(screenSize)
+    ms_elapsed = 0
+    gameClock = pygame.time.Clock()
+    while True:
+        print "Renderer loop"
+        
+        print "starting Event manager"
+        Process(target=eventManager,args=(pygame.event.get(),)).start()
+        
+        screen.fill((randint(0,255),0,0))
+        pygame.display.flip()
+        print ms_elapsed
+        ms_elapsed = gameClock.tick()
+
+if __name__ == '__main__':
+    print "starting Renderer"
+    Process(target=renderer).start()
