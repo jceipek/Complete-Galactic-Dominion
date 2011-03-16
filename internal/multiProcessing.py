793eff79b712d4c1304033db249d1f23522095a9
Julian Ceipek <julian.ceipek@gmail.com>
2011-03-15 20:43:18 -0400

    Patrick added threading, broken on OSX

--- internal/multiProcessing.py
+++ internal/multiProcessing.py
@@ -1,7 +1,7 @@
 from random import randint
-
+import threading
 #import pygame
-from multiprocessing import Process, Pipe, Lock
+#from multiprocessing import Process, Pipe, Lock
 
 
 #pygame.init()
@@ -10,26 +10,28 @@ from multiprocessing import Process, Pipe, Lock
 def eventManager(events):
 	print "Event Manager loop"
 	for event in events:
-				print event
+		print event
 
 def renderer():
-    import pygame
-    screenSize = (width, height) = (640, 480)
-    pygame.init()
-    screen = pygame.display.set_mode(screenSize)
-    ms_elapsed = 0
-    gameClock = pygame.time.Clock()
-    while True:
-        print "Renderer loop"
-        
-        print "starting Event manager"
-        Process(target=eventManager,args=(pygame.event.get(),)).start()
-        
-        screen.fill((randint(0,255),0,0))
-        pygame.display.flip()
-        print ms_elapsed
-        ms_elapsed = gameClock.tick()
+   import pygame
+   screenSize = (width, height) = (640, 480)
+   pygame.init()
+   screen = pygame.display.set_mode(screenSize)
+   ms_elapsed = 0
+   gameClock = pygame.time.Clock()
+   while True:
+       print "Renderer loop"
+       
+       print "starting Event manager"
+       threading.Thread(target=eventManager,args=(pygame.event.get(),)).start()
+       
+       screen.fill((randint(0,255),0,0))
+       pygame.display.flip()
+       print ms_elapsed
+       ms_elapsed = gameClock.tick()
 
 if __name__ == '__main__':
-    print "starting Renderer"
-    Process(target=renderer).start()
+   print "starting Renderer"
+   a=threading.Thread(target=renderer)
+   a.start()
+   a.join()
