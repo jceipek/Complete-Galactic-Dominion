58a991b7d66325127b265143e3d4d588eaae44b1
Jared Kirschner <stockstalker@gmail.com>
2011-03-21 20:11:06 -0400

    DrawableObject updated.  Now contains a test to run if main to show it works.

--- DrawableObject.py
+++ DrawableObject.py
@@ -10,9 +10,8 @@ class DrawableObject():
 
     # First loadImage method
     def loadImage(self, name, colorkey=None):
-        fullname = os.path.join('data', name)
         try:
-            image = pygame.image.load(fullname)
+            image = pygame.image.load(name)
         except pygame.error, message:
             print 'Cannot load image:', name
             raise SystemExit, message
@@ -20,7 +19,22 @@ class DrawableObject():
         if colorkey is not None:
             if colorkey is -1:
                 colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, RLEACCEL)
+            image.set_colorkey(colorkey)
         return image, image.get_rect()
-        
+
+if __name__ == "__main__":
+    screenSize = (width, height) = (1024, 768)
+    screenLoc = [0.0, 0.0]
+
+    RUNNING = True
+    pygame.init()
+    screen = pygame.display.set_mode(screenSize)
+    screenZone = screen.get_rect()
+    
+    a = DrawableObject('ball.png',(255,255,255))
+    
+    while RUNNING:
+        pygame.init()
+        screen.blit(a.image,a.rect)
+        pygame.display.flip()
     
