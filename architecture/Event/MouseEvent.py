8d38ce0dff5de3f498a4ba4f2deba444df56599a
Julian Ceipek <julian.ceipek@gmail.com>
2011-04-08 15:01:28 -0400

    Cleaned up some code

--- architecture/Event/MouseEvent.py
+++ architecture/Event/MouseEvent.py
@@ -7,28 +7,6 @@ class MouseLocals:
     RIGHT_CLICK = 3
     SCROLL_UP = 4
     SCROLL_DOWN = 5
-
-'''
-class MouseClickedEvent(Event):
-    """
-    Basic mouse event.
-    
-    #Attributes:
-    #   pos = (x,y)
-    #   state = pressed or released ENUM
-    #   buttonID = button ENUM
-    """
-
-    def __init__(self,posxy,state,buttonId):
-        Event.__init__(self)
-        self.pos = posxy
-        self.state = state
-        self.buttonId = buttonId
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"+\
-        "\tButton State: "+str(self.state)+"\n"+\
-        "\tButton Pressed: "+str(self.buttonId)+"\n"
-        
-'''
         
 class MouseMovedEvent(Event):
     """
@@ -41,64 +19,64 @@ class MouseMovedEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"
         
 class SelectionEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"
         
 class SingleAddSelectionEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"
 
 class SingleAddSelectionEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"
         
 class DragBeganEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"        
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"        
         
 class AddDragBeganEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"      
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"      
         
 class DragEvent(Event):
     def __init__(self,startxy,currxy):
         Event.__init__(self)
         self.start = startxy;
         self.curr = currxy;
-        self.verboseInfo = "\tStartPos: "+str(self.start)+"\n"+\
-                           "\tCurrPos: "+str(self.curr)+"\n"
+        self.verboseInfo = "\tStartPos: " + str(self.start) + "\n" + \
+                           "\tCurrPos: " + str(self.curr) + "\n"
 
 class DragCompletedEvent(Event):
     def __init__(self,startxy,endxy):
         Event.__init__(self)
         self.start = startxy
         self.curr = endxy
-        self.verboseInfo = "\tStartPos: "+str(self.start)+"\n"+\
-                           "\tEndPos: "+str(self.curr)+"\n"
+        self.verboseInfo = "\tStartPos: " + str(self.start) + "\n" + \
+                           "\tEndPos: " + str(self.curr) + "\n"
  
 class AddDragCompletedEvent(Event):
     def __init__(self,startxy,endxy):
         Event.__init__(self)
         self.start = startxy
         self.end = endxy
-        self.verboseInfo = "\tStartPos: "+str(self.start)+"\n"+\
-                           "\tEndPos: "+str(self.end)+"\n"
+        self.verboseInfo = "\tStartPos: " + str(self.start) + "\n" + \
+                           "\tEndPos: " + str(self.end) + "\n"
 
 class SetDestinationEvent(Event):
     def __init__(self,posxy):
         Event.__init__(self)
         self.pos = posxy
-        self.verboseInfo = "\tPosition: "+str(self.pos)+"\n"
+        self.verboseInfo = "\tPosition: " + str(self.pos) + "\n"
