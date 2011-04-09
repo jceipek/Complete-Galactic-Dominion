26a26dc61a4ad2abcdeff6507b52e8891d69daf7
Julian Ceipek <julian.ceipek@gmail.com>
2011-04-08 20:41:00 -0400

    Abstracted select closest. Broke selection in badly designed cases.

--- architecture/Viewport.py
+++ architecture/Viewport.py
@@ -135,7 +135,6 @@ class Viewport(object):  #SHOULD PROBABLY INHERIT FROM DRAWABLE OBJECT
         """
         
         pos = event.pos
-        #FIXME - VERY INEFFICIENT/UGLY IMPLEMENTATION RIGHT NOW
         def distBetween(p1,p2):
             return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**.5
 
