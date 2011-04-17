847bd9b1bf3e219366c8e5184ec5e13821fd0451
Jared Kirschner <stockstalker@gmail.com>
2011-04-16 23:47:06 -0400

    Now with more structure!

---
+++ architecture/Structure.py
@@ -0,0 +1,28 @@
+from Unit import Builder, Unit
+from Entity import Locals
+
+class Structure(Builder):
+    """Defines structues which are built by units"""
+
+    def __init__(self, imagePath, x, y, world, colorkey=None,
+                 description = 'No information available.'):
+        Builder.__init__(self, imagePath, x, y, world, colorkey, description)
+
+        # first to define these attributes
+        # sets where a new entity should be constructed
+        self.buildX,self.buildY = self.rect.center
+
+        self.status = Locals.IDLE
+        self.buildDict = {
+            Unit: 
+                lambda : Unit('testCraft.png',self.buildX,self.buildY,self.world,'alpha','A Unit.')
+            }
+
+'''
+class TownCenter(Structure):
+	"""defines Town Center structure """
+	resourcesRequired=None
+	timeToBuild=0
+	def __init__(self, imagePath, colorkey=None):
+		Structure.__init__(self, imagePath, colorkey)
+'''
