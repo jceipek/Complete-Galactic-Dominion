ccf936eef6c9d9664f749c023c7e269ead116cf5
Berit Johnson <berit.johnson@students.olin.edu>
2011-04-25 17:30:37 -0400

    Viewport fills window now.

--- architecture/HUD.py
+++ architecture/HUD.py
@@ -4,16 +4,10 @@ from HUDElements import DescriptionBox, SelectedUnitBar, Notification, Notificat
 from GameData import Locals
 
 class HUD(object):
-    def __init__(self, loc, size):
-        self.loc=loc
-        self.size=size
-        self.surface=pygame.Surface(size)
+    def __init__(self):
         self.descBox = DescriptionBox()
         self.selectedUnitBar = SelectedUnitBar()
-        self.rect = pygame.Rect(self.loc,self.size)
-        self.width=200
         self.viewport=None
-        self.infoRect=pygame.Rect((0,20), (self.size[0], self.size[1]-20))
         self.note=NotificationList()
         self.note.add(Notification('Imma notify you'))
         self.note.add(Notification('This is a slightly shorter lasting notification', time=2))
@@ -21,7 +15,6 @@ class HUD(object):
         
     def draw(self, displaySurface):
         self.drawSelected()
-        displaySurface.blit(self.surface, (self.loc,self.size))
         self.selectedUnitBar.draw(displaySurface)
         self.descBox.draw(displaySurface)
         self.note.draw(displaySurface)
