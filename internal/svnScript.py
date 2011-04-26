bdbad7c5dfdaa92e9b7496cd02b360ca65381233
Julian Ceipek <julian.ceipek@gmail.com>
2011-04-25 23:35:33 -0400

    Fixed svnScript

--- internal/svnScript.py
+++ internal/svnScript.py
@@ -2,7 +2,7 @@ import os
 
 #os.popen('rm -rf /home/is/subversionRepo/complete-galactic-dominion/*')
 os.popen('cp -r /home/is/.hudson/jobs/complete-galactic-dominion/workspace/* /home/is/subversionRepo/complete-galactic-dominion/')
-os.popen('svn add /home/is/subversionRepo/complete-galactic-dominion/*')
+os.popen('svn add /home/is/subversionRepo/complete-galactic-dominion/* --force')
 gitres = os.popen('git log -1')
 gitcommit = gitres.read()
 gitres.close()
