d813d5ad9baf0540082013e336d309ae0cee0629
Julian Ceipek <julian.ceipek@gmail.com>
2011-05-05 20:20:22 -0400

    Moved Berit's E&M project out of CGD

--- architecture/client.py
+++ architecture/client.py
@@ -4,10 +4,10 @@ B{RUN ME FIRST!}
 ######################\n
 #       Client       #\n
 ######################\n
-This file is used to set up everything that clients can do and begins the 
+This file is used to set up everything that clients can do and begins the
 program.
 
-B{FIXME:}We might want to move some of this code into a function that sets up 
+B{FIXME:}We might want to move some of this code into a function that sets up
 servers and clients as needed. For now, no servers have been created.
 """
 
@@ -34,7 +34,7 @@ from WorldManipulator import WorldManipulator
 def init(host='localhost'):
     """
     Game initialization function.
-        1. Creates a L{Debugger} (debugger) and L{EventTimer} (eventTimer) and 
+        1. Creates a L{Debugger} (debugger) and L{EventTimer} (eventTimer) and
            stores references to them in an instance of L{EventManager}.
         2. Several listeners are started and registered with eventManager:
             - Instance of L{Universe} (universe)
@@ -46,15 +46,15 @@ def init(host='localhost'):
 
     debugger = Debugger()
     eventTimer = EventTimer()
-    
+
     #Create the event manager for low-level events
     eventManager = Manager(eventTimer,debugger) #FIXME: more specific manager\
                                                 #classes will be needed later?
-    Entity.manager = eventManager                                            
-                                               
+    Entity.manager = eventManager
+
     #Create the occurence manager for high-level events (same across client and server)
     #FIXME: NOT YET IMPLEMENTED
-    
+
     #THIS WILL BE CHANGED LATER TO ACCOUNT FOR LOADING, ETC.
 
     networked = True
@@ -62,15 +62,15 @@ def init(host='localhost'):
     # World w is set to the activeWorld of the universe
     universe = Universe(eventManager)
     ui = UserInterface(eventManager,universe.activeWorld,None)
-    
+
     gameWindow = Window(eventManager,width=1024,height=768)
     gameWindow.fullscreenMode = False
     gameWindow.updateScreenMode()
-    
+
     w = World(universe)
     #universe.changeWorld(w)
-    
-    try:                                            
+
+    try:
         client = GameClient(eventManager,host=host,port=1567)
         while client.ID == None:
             import time
@@ -81,30 +81,30 @@ def init(host='localhost'):
 
     except:
         networked = False
-        
+
     if not networked:
         # Initialize 25 entities in World w
         # Initialize a TestTownCenter
         clientID = GameClient.ID = 1
         ui.setClientID(clientID)
         eventManager.post(Event.NewPlayerEvent(clientID))
-        
+
     else:
         clientID = client.ID
         ui.setClientID(clientID)
-    
+
     wManipulator = WorldManipulator(eventManager,w,networked,gameClientID = clientID)
-    
+
     #Notify the manager that the window should start to accept input:
     eventManager.post(Event.StartEvent())
-    
+
     return eventManager.eventTypesToListeners
 
 if __name__ == '__main__':
     #FIXME: Very little implemented here.
     #Connect to server
-    
-    import sys
+
+    import sys,cProfile
 
     theHost = 'localhost'
     if (len(sys.argv) == 2):
@@ -113,5 +113,6 @@ if __name__ == '__main__':
         print "Usage: python client.py ipAddress"
         print "Reverting to localhost..."
         theHost = 'localhost'
-    
-    eTypestoListeners = init(theHost)
+
+    cProfile.run('eTypestoListeners = init(theHost)','ProgramProfile.txt')
+    #eTypestoListeners = init(theHost)
